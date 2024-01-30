8fc65f1f8d2efa74ee380b76e4fdd890aee13766
Tornike <itorn9777@gmail.com>
2024-01-30 17:54:33 +0400

    add footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,17 @@ export default function Footer() {
     <footer className="Footer">
       <div className="container">
         <div className="Footer-wrapper flex items-center justify-center py-4">
-          Created by Tornike Epitashvili
+          <div>
+            <span>Created by </span>
+            <a
+              href="https://github.com/Makinloot"
+              rel="noreferrer"
+              target="_blank"
+              className="underline"
+            >
+              Tornike Epitashvili
+            </a>
+          </div>
         </div>
       </div>
     </footer>
