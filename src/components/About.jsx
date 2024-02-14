0a0a7cb322013f2636e712388f0771a1438302cf
Tornike <itorn9777@gmail.com>
2024-02-14 19:10:03 +0400

    update about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ export default function About() {
               initial={{ opacity: 0, x: -500 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.7, delay: 0.5 }}
-              className="self-center"
+              className=""
             >
               <img
                 className="w-full max-w-[350px] rounded-lg"
@@ -33,19 +33,32 @@ export default function About() {
               initial={{ opacity: 0, x: 500 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.7, delay: 0.5 }}
-              className="my-8 max-w-[600px] flex-1 lg:my-0 lg:ml-8 "
+              className="my-8 max-w-[600px] flex-1 items-start lg:my-0 lg:ml-8 "
             >
-              <p className="lg:text-lg">
-                Cascade records is a techno-ambient label founded in the capital
-                of Georgia, Tbilisi, the author and founder of the idea is the
-                solo mysterious project artist IV Georgian artist, who from 2020
-                presents projects in the dark-ambient genre such as Cascade
-                promo series, Cascade demo series.. Cascade family in 2021-22
-                After the expansion, the label continued to operate in
-                Spain.Where Cascade Records new resident Ramzi, in collaboration
-                with founder IV, presents B2B projects, Cascade sellers series,
-                Cascade podcast series, and the label also presents epic solo
-                podcasts by legendary techno artists from various labels
+              <p className="indent-8 lg:text-lg">
+                CASCADE RECORDS is an underground techno label. It was founded
+                in early 2020, in the suburbs of Tbilisi by techno artist IV.
+                Production in 2020 moved to spain in province of Valencia,
+                Castellón. In the end of 2020, unknown until now techno artist
+                by nickname Ramzi became first resident of label, Ramzi artist,
+                which combines Techno and Hardtechno by solo projects like
+                Techno ACT and Techno Revelation. After this Project CASCADE
+                became more active in production to be more oriented on
+                listeners and for techno industry.
+              </p>
+              <p className="indent-8 lg:text-lg">
+                CASCADE has organized view how should underground techno events
+                work. Label with new projects: Selector Series, Promo Series,
+                Down Industrial, Demo Series, aims to share underground techno
+                vision to people and techno scenes in different locations.
+              </p>
+              <p className="indent-8 lg:text-lg">
+                One of the main component of Label Acid-Industrial is acid
+                effects, brutal industrial sounds with ambient, dark background.
+                For Record Projects and future plans, it is crucial from
+                internet platforms, events, radio channels and Spain or any
+                different place to move to boiling techno ravers and with them
+                join this culture with active movement.
               </p>
               <div className="social-medias mt-6">
                 <motion.h3
